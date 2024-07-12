7dc4af1bb2f89c5d0442c894add38a2114ab8442
pratiksha_chaure <pratikshachaure381@gmail.com>
2024-07-12 03:19:04 -0700

    string reversed

--- 80_reverse_string.js
+++ 80_reverse_string.js
@@ -1,5 +1,5 @@
 // 093. Write a program to reverse the given string in
-        let string="I am reverse string";
+        let string="I am reverse string and i want to say i am happy with it";
         let reverse=string.split('').reverse().join('')
         console.log(reverse)
  
