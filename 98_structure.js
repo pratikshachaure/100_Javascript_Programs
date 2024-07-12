783db41dd100705b82463d4abb501dca08ea3e42
pratiksha_chaure <pratikshachaure381@gmail.com>
2024-07-12 02:57:54 -0700

    hellow

---
+++ 98_structure.js
@@ -0,0 +1,16 @@
+// Create an object with properties
+let person1 = {
+    name: "",
+    citNo: 0,
+    salary: 0.0
+  };
+  
+  // Assign values to properties
+  person1.name = "Pratiksha Chaure ";
+  person1.citNo = 1984;
+  person1.salary = 2500.0;
+  
+  // Print object properties
+  console.log(`Name: ${person1.name}`);
+  console.log(`Citizenship No.: ${person1.citNo}`);
+  console.log(`Salary: ${person1.salary.toFixed(2)}`);
